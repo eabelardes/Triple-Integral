9f1a6eb771775cb24afb7127a97822a2a2d26df4
eabelardes <eabelardes@unal.edu.co>
2025-11-18 22:53:02 -0500

    Create densidades.h

---
+++ src/include/densidades.h
@@ -0,0 +1,20 @@
+#ifndef DENSIDADES_H
+#define DENSIDADES_H
+
+double densidad_constante(double, double, double, double, double, double);
+double densidad_lineal(double, double, double, double, double, double);
+double densidad_gaussiana(double, double, double, double, double, double);
+
+double densidad_constante_x(double, double, double, double, double, double);
+double densidad_constante_y(double, double, double, double, double, double);
+double densidad_constante_z(double, double, double, double, double, double);
+
+double densidad_lineal_x(double, double, double, double, double, double);
+double densidad_lineal_y(double, double, double, double, double, double);
+double densidad_lineal_z(double, double, double, double, double, double);
+
+double densidad_gaussiana_x(double, double, double, double, double, double);
+double densidad_gaussiana_y(double, double, double, double, double, double);
+double densidad_gaussiana_z(double, double, double, double, double, double);
+
+#endif
